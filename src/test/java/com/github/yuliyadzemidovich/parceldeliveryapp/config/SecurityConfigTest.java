8af7ed4eb8b1d76fc2cae9bd4c68609e1d02ae1e
Yuliya Dzemidovich <yuliya.dzemidovich@gmail.com>
2023-05-07 23:51:22 +0400

    fixed H2 database auto setup in tests

--- src/test/java/com/github/yuliyadzemidovich/parceldeliveryapp/config/SecurityConfigTest.java
+++ src/test/java/com/github/yuliyadzemidovich/parceldeliveryapp/config/SecurityConfigTest.java
@@ -2,12 +2,14 @@ package com.github.yuliyadzemidovich.parceldeliveryapp.config;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 @SpringBootTest
+@AutoConfigureTestDatabase // use in-memory H2 instead of init Datasource bean with connection to the real database
 class SecurityConfigTest {
 
     @Autowired
